9bdfbabd2ec494068977888b8327c22fb600bece
Stefan Kuznetsov <skuznetsov2@my.bcit.ca>
2021-03-01 00:47:16 -0800

    Added the lyrics screen

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LoginScreen from './src/screens/LoginScreen';
+import LyricsScreen from './src/screens/LyricsScreen';
 
 import { save } from './src/api/storage';
 
+const Stack = createStackNavigator();
+
 export default function App() {
-  return <LoginScreen saveToken={save} />
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name='Root'>
+            {(props) => <LoginScreen {...props} saveToken={save} />}
+          </Stack.Screen>
+          <Stack.Screen name='Lyrics' component={LyricsScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
 }

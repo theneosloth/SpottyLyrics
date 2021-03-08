d79dd19607ceac1171217b3cbaea92dd03ae84c2
Stefan Kuznetsov <skuznetsov2@my.bcit.ca>
2021-03-08 01:52:25 -0800

    Added bottom tab navigation

--- src/components/navigation/BottomTabNavigator.js
+++ src/components/navigation/BottomTabNavigator.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import LoginScreen from '../../screens/LoginScreen';
 import LyricsScreen from '../../screens/LyricsScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
 
 const BottomTab = createBottomTabNavigator();
 
@@ -13,8 +15,26 @@ export default function BottomTabNavigator() {
   return (
     <NavigationContainer>
       <BottomTab.Navigator>
-        <BottomTab.Screen name="Login" component={LoginScreen} />
-        <BottomTab.Screen name="Lyrics" component={LyricsScreen} />
+        <BottomTab.Screen options={
+                            {
+                              tabBarVisible: false,
+                              tabBarIcon: () => {
+                                return <EvilIcons name="user" size={24} color="black" />
+                              }
+                            }
+                          }
+                          name="Login"
+                          component={LoginScreen} />
+
+        <BottomTab.Screen options = {
+                            {
+                              tabBarIcon: () => {
+                                return <SimpleLineIcons name="notebook" size={24} color="black" />
+                              }
+                            }
+                          }
+                          name="Lyrics"
+                          component={LyricsScreen} />
       </BottomTab.Navigator>
     </NavigationContainer>
   );
